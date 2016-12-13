947b89a7f9c6960b3c4cd4e81b2604fc12330e36
aqb_arindam <aqb_arindam@393a1347-5598-4bb1-a8be-7e581ccc0dc9>
2016-12-13 11:36:55 +0000

    Changes for responsive

--- branches/version2_0_1/MediCuris/public_html/includes/footer.php
+++ branches/version2_0_1/MediCuris/public_html/includes/footer.php
@@ -1,4 +1,5 @@
 <footer id="footer"  class="dark_background">
+<!--
 	<section class="container footer-in">
 		<div class="col-md-3">
 			<div class="widget">
@@ -15,16 +16,33 @@
 				</div>
 			</div>
 		</div>
-
-
-
 	</section>
+-->
 	<!-- end-footer-in -->
 	<section class="footbot">
 		<div class="container">
-			<div style="text-align: left;" class="col-md-12">
-				<a href="/"> <img id="footer-logo" src="images/logo1.png" alt="orinoco logo"> &#0169 &nbsp;2016. All
-				rights reserved.&nbsp; | by Orinoco Inc.</a>
+			<div class="col-sm-3 footerlogo">
+				<a href="/"> <img id="footer-logo" src="images/logo1.png" alt="orinoco logo"></a>
+			</div>
+            <div class="col-md-4">
+                <div class="widget">
+                    <div class="menu-footer1-container">
+                        <ul class="footermenu">
+                            <li>
+                                <a>medicuris.com</a>
+                            </li>
+
+                            <li>
+                                <a class="page-scroll" href=<?php echoActiveClassIfRequestMatches("index","contact") ?>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+			<div class="col-sm-5">
+                <div class="copyright">
+				    <a href="/">&#0169 &nbsp;2016. All rights reserved.&nbsp; | by Orinoco Inc.</a>
+                </div>
 			</div>
 		</div>
 	</section>
